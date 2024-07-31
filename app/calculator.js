fc49068cc8390e100ad54a5af76083f8fdb10a0c
Channa2002 <channabasava836@gmail.com>
2024-07-31 20:21:56 +0530

    solution

--- app/calculator.js
+++ app/calculator.js
@@ -1,4 +1,31 @@
 exports.calculate = function(expression) {
-  return 0;
+  const numbers = expression.trim().split(' ');
+  const stack = [];
+
+  for(let i = numbers.length - 1 ; i >= 0; i--) {
+    const token = numbers[i];
+    if (!isNaN(token)) {
+      stack.push(parseFloat(token));
+    } else {
+      const num1 = stack.pop();
+      const num2 = stack.pop();
+      
+      if(token === '+') {
+           stack.push(num1 + num2);
+      } else if (token === '-') {
+        stack.push(num1 - num2);
+      } else if (token === '*') {
+        stack.push(num1 * num2);
+      } else if(token === '/') {
+        stack.push(num1 / num2);
+      } else {
+        throw new Error('Invalid operator');
+      }
+    }
+  }
+
+  const result = stack.pop();
+  return result;
+
 }
 
